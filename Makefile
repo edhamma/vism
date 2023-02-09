e849c2a524052a4b7b96b158e6e1cccd2d62813b
Václav Šmilauer <eu@doxos.eu>
2023-02-09 12:49:24 +0100

    small updates

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 build:
 	latexmk -cd latex/vism.tex
-	make -C sphinx html
+	make -C sphinx html epub
 	cd latex; plastex -c plastex.ini vism.tex
 deploy:
 	git checkout gh-pages
-	git add latex/html latex/vism.pdf sphinx/build/html
+	git add index.html latex/html latex/vism.pdf sphinx/build/html sphinx/build/epub
 	git commit -m 'updated build'
 	git push origin gh-pages
 	git checkout main
-	# evince vism.pdf &
--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,9 +7,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Visuddhimagga'
-copyright = '2023, Buddhaghosa'
+copyright = 'The Buddhist Publication Society'
 author = 'Buddhaghosa'
-release = '(decompiled)'
+release = '(WIP)'
+version = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,9 +22,29 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+html_theme_options = {
+    'github_user':'eudoxos',
+    'github_repo':'vism',
+    'github_banner':'true',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+epub_title = project
+epub_author = author
+epub_publisher = 'The Buddhist Publication Society'
+epub_copyright = copyright
+epub_cover = ('_static/cover.jpg','epub-cover.html')
+epub_language='en'
+epub_basename='vism'
+epub_use_index=False
+epub_scheme='ISBN'
+epub_identifier = '978-955-24-0023-6'
+# A unique identification for the text.
+epub_uid = 'visuddhimagga'
+
