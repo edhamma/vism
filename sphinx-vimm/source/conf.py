416ca57ab09fe757ad5876ecd84f77f4af3917f7
eudoxos <github@doxos.eu>
2024-03-11 13:27:37 +0000

    Deploying to gh-pages from @ edhamma/vism@4a272065b498a8b6aede2aef4fbdaa8c4fdce005 🚀

---
+++ sphinx-vimm/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Vimuttimagga — Digital'
+copyright = 'Digital'
+author = 'The Arahant Upatissa'
+
+import git, urllib.parse
+vismCommit=(head:=git.Repo(search_parent_directories=True).head).object.hexsha[:7]
+
+release = vismCommit
+version = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo']
+todo_include_todos=True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+html_theme_options = {
+    'github_user':'eudoxos',
+    'github_repo':'vism',
+    'github_banner':'true',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+epub_title = project
+epub_author = author
+epub_publisher = '~~ eudoxos.github.io/vism WIP ~~'
+epub_copyright = copyright
+epub_cover = ('_static/cover.jpg','epub-cover.html')
+epub_language='en'
+epub_basename='vimm'
+epub_use_index=False
+epub_scheme='ISBN'
+epub_identifier = '978-955-24-0023-6'
+# A unique identification for the text.
+epub_uid = 'vimuttimagga'
+
---
+++ sphinx/source/part-I.rst
@@ -0,0 +1,14 @@
+
+
+.. _pI:
+
+Part I. Virtue (*Sīla*\ )
+*************************
+
+.. toctree::
+   :maxdepth: 6
+
+
+   
+   ch-01.rst
+   ch-02.rst
